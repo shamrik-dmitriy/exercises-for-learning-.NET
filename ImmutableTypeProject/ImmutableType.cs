62eaa3e7b1795f545f0100a078b548e70f79a3f1
Шамрик Дмитрий Леонидович <dshamrick@gemsdev.com>
2021-12-22 10:55:17 +0600

    Added read-only properties and sealed modificator in class

--- ImmutableTypeProject/ImmutableType.cs
+++ ImmutableTypeProject/ImmutableType.cs
@@ -1,7 +1,18 @@
-﻿namespace ImmutableTypeProject
+﻿using System;
+
+namespace ImmutableTypeProject
 {
-    public class ImmutableType
+    public sealed class ImmutableType
     {
-        
+        public string Name { get; }
+        public bool Parameter1 { get; }
+        public int Parameter2 { get; }
+
+        public ImmutableType(string name, bool parameter1, int parameter2)
+        {
+            Name = name;
+            Parameter1 = parameter1;
+            Parameter2 = parameter2;
+        }
     }
 }

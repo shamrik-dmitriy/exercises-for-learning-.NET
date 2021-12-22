9fd94fe9f60b68f31e46785c2b777a6737db5b81
Шамрик Дмитрий Леонидович <dshamrick@gemsdev.com>
2021-12-22 12:39:47 +0600

    Added immutable class

---
+++ ImmutableTypeProject/ImmutablePerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ImmutableTypeProject
+{
+    public sealed class ImmutablePerson
+    {
+        public string Name { get; }
+        public bool Gender { get; }
+        public int Parameter2 { get; }
+
+        public ImmutablePerson(string name, bool gender, int parameter2)
+        {
+            Name = name;
+            Gender = gender;
+            Parameter2 = parameter2;
+        }
+
+        public ImmutablePerson ChangeName(string name)
+        {
+            return new ImmutablePerson(name, Gender, Parameter2);
+        }
+
+        public ImmutablePerson ChangeGender(bool gender)
+        {
+            return new ImmutablePerson(Name, gender, Parameter2);
+        }
+
+        public ImmutablePerson ChangeParameter1(int age)
+        {
+            return new ImmutablePerson(Name, Gender, age);
+        }
+    }
+}
--- ImmutableTypeProject/Program.cs
+++ ImmutableTypeProject/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace ImmutableTypeProject
+﻿using System;
+
+namespace ImmutableTypeProject
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            ImmutablePerson immutablePerson = new ImmutablePerson("Name1", true, 12);
+            immutablePerson.ChangeGender(false);
         }
     }
 }

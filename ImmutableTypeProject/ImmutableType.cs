7c09f45ce83ae58e4a1005204f4cf1de69c894c6
Шамрик Дмитрий Леонидович <dshamrick@gemsdev.com>
2021-12-22 10:35:12 +0600

    Create solution and added new project for ex 1

---
+++ ImmutableTypeProject/ImmutableType.cs
@@ -0,0 +1,7 @@
+﻿namespace ImmutableTypeProject
+{
+    public class ImmutableType
+    {
+        
+    }
+}
---
+++ ImmutableTypeProject/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ImmutableTypeProject
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
